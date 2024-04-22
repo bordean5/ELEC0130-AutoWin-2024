8d73de28476f6df8c88ee9c40aade0f28815fbf5
David GERARD <56201515+David-GERARD@users.noreply.github.com>
2024-04-22 17:07:22 +0100

    Update addToBasket.py to latest version

--- AWS/addToBasket.py
+++ AWS/addToBasket.py
@@ -38,42 +38,32 @@ def lambda_handler(event, context):
     # -> query an API or a database to get the article name and its properties 
     # -> format the properties to be added as a JSON 
     
-    data = requests.get(f"https://world.openfoodfacts.net/api/v2/product/5020364010144?fields=product_name,nutriments,quantity")
+    data = requests.get(f"https://world.openfoodfacts.net/api/v2/product/{barcode_number}?fields=product_name,nutriments,quantity")
     if data.status_code == 200:
         d = data.json()
-        #print (d)
         product = d.get('product', {})
         article_name = product.get("product_name",'')
         quantity = product.get('quantity', '')
         nutriments = product.get('nutriments', {})
-        print(product)
+        
         quantity_g = 0
         try:
             quantity_g = float(quantity.replace('g', '').strip())
         except Exception as e:
-             # Send notification to scanner
-            response_Lambda = client.invoke(
-                            FunctionName='sendUserNotificationToScanner',
-                            InvocationType='Event',
-                            Payload=json.dumps({
-                                'basket_id':basket_id,
-                                'notification': "addToBasket Error: Error converting quantity " + str(e)
-                            })
-            )
             print('Error converting quantity:', e)
         
         total_nutriments = {}
-        #if quantity_g:
-        for key, value in nutriments.items():
-            if key.endswith('_100g'):
-                nutriments_per_100g = value
-                total_nutriments[key] = nutriments_per_100g
-    
-        total_energy = total_nutriments.get('energy-kcal_100g', 'Unknown')/100 * quantity_g
-        total_fat = total_nutriments.get('fat_100g', 'Unknown')/100 * quantity_g
-        total_carbohydrates = total_nutriments.get('carbohydrates_100g', 'Unknown')/100 * quantity_g
-        total_proteins = total_nutriments.get('proteins_100g', 'Unknown')/100 * quantity_g
-        total_salt = total_nutriments.get('salt_100g', 'Unknown')/100 * quantity_g
+        if quantity_g:
+            for key, value in nutriments.items():
+                if key.endswith('_100g'):
+                    nutriments_per_100g = value
+                    total_nutriments[key] = (nutriments_per_100g / 100) * quantity_g
+        
+        total_energy = total_nutriments.get('energy-kcal_100g', 'Unknown')
+        total_fat = total_nutriments.get('fat_100g', 'Unknown')
+        total_carbohydrates = total_nutriments.get('carbohydrates_100g', 'Unknown')
+        total_proteins = total_nutriments.get('proteins_100g', 'Unknown')
+        total_salt = total_nutriments.get('salt_100g', 'Unknown')
         
         energy = total_energy
         fat = total_fat
@@ -93,33 +83,21 @@ def lambda_handler(event, context):
             "scan_date":scan_date 
         })
         
-        try:
-            # Update the item in DynamoDB to add a new element to the 'basket_content' string set
-            response = table.update_item(
-                Key={
-                    'basket_id': basket_id
-                },
-                UpdateExpression="ADD basket_content :new_content",
-                ExpressionAttributeValues={
-                    ':new_content': {new_content}
-                },
-                ReturnValues="UPDATED_NEW"
-            )
-        except Exception as e:
-            error = json.dumps({"error":str(e)})
-            return {'statusCode': 500, 'body': error}
-    
-    
         
-        # Send notification to scanner
-        response_Lambda2 = client.invoke(
-                        FunctionName='computeNutritionValues',
-                        InvocationType='Event',
-                        Payload=json.dumps({
-                            'basket_id':basket_id
-                        })
+        # Update the item in DynamoDB to add a new element to the 'basket_content' string set
+        response = table.update_item(
+            Key={
+                'basket_id': basket_id
+            },
+            UpdateExpression="ADD basket_content :new_content",
+            ExpressionAttributeValues={
+                ':new_content': {new_content}
+            },
+            ReturnValues="UPDATED_NEW"
         )
-        
+    
+    
+
         # Send notification to scanner
         response_Lambda = client.invoke(
                         FunctionName='sendUserNotificationToScanner',
@@ -130,6 +108,15 @@ def lambda_handler(event, context):
                         })
         )
         
+        # Send notification to scanner
+        response_Lambda2 = client.invoke(
+                        FunctionName='computeNutritionValues',
+                        InvocationType='Event',
+                        Payload=json.dumps({
+                            'basket_id':basket_id
+                        })
+        )
+        
     else: 
          # Send notification to scanner
         response_Lambda = client.invoke(
@@ -137,7 +124,7 @@ def lambda_handler(event, context):
                         InvocationType='Event',
                         Payload=json.dumps({
                             'basket_id':basket_id,
-                            'notification': "API Error."
+                            'notification': "API Error"
                         })
         )
         
